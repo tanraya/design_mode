9f1e5e0874a18e9884b3f80e1aab15600d5fc41e
RubberRails <demerest@gmail.com>
2011-02-02 17:18:36 +0500

    Added useful comment 2

--- js/design_mode/design_mode.js
+++ js/design_mode/design_mode.js
@@ -1,5 +1,6 @@
 // Editor general class
-//  It's useful: http://accessgarage.wordpress.com/2009/05/08/how-to-hack-your-app-to-make-contenteditable-work/
+// It's useful: http://accessgarage.wordpress.com/2009/05/08/how-to-hack-your-app-to-make-contenteditable-work/
+// http://blog.whatwg.org/the-road-to-html-5-contenteditable
 var DesignMode = new Class({
     Implements : [Events, Options],
 
